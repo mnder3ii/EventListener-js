d72fb981508ab870ed41c1a88fe0dab44b358635
mno90 <131266070+mnder3ii@users.noreply.github.com>
2023-06-19 05:05:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+var nmofbutns = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < nmofbutns; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () { 
+
+        var butninval = this.innerHTML;
+
+        makeing_sound(butninval);
+
+    });
+
+}
+
+
+document.addEventListener("keydown", function (event) {
+
+    makeing_sound(event.key);
+
+});
+
+
+function makeing_sound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("./tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio("./tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio("./tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio("./tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio("./snare.mp3");
+            snare.play();
+        break;
+
+        case "k":
+            var crash = new Audio("./crash.mp3");
+            crash.play();
+        break;
+
+        case "l":
+            var kick_bass = new Audio("./kick-bass.mp3");
+            kick_bass.play();
+        break;
+
+    
+        default: console.log(butninval);
+            break;
+    }
+    
+}
+
+
